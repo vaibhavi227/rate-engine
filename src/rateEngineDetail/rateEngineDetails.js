0d5fc32a79e08123fe8298e44a756aabfb3df913
vaibhavi227 <vaibhavibawne227@gmail.com>
2023-12-01 12:33:20 +0530

    deleted inline css and added internal css

--- src/rateEngineDetail/rateEngineDetails.js
+++ src/rateEngineDetail/rateEngineDetails.js
@@ -1,40 +1,53 @@
-import {
-  FormControl,
-  Grid,
-  Input,
-  InputLabel,
-  Typography,
-} from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { ShipmentDetails } from "./rateEngineDetailsComponents";
 import RateEngineContainerDetail from "./ContainerDetails";
+
 export default function DetailsComponentMain() {
+  const styles = {
+    gridContainer: {
+      display: "flex",
+      justifyContent: "flex-end",
+    },
+    quotationDetails: {
+      color: "#f96332",
+    },
+    activeSpan: {
+      color: "green",
+    },
+    shipmentDetails: {
+      color: "#3f51b5",
+      mt: 2,
+      mb: 2,
+      fontSize: "17px",
+    },
+  };
+
   return (
     <div>
       <Grid container spacing={2}>
         <Grid item xs={6}>
-          <Typography variant="h6" sx={{color: "#f96332"}}>
+          <Typography variant="h6" sx={styles.quotationDetails}>
             Quotation Details
           </Typography>
         </Grid>
-        <Grid item xs={6} sx={{ textAlign: "right" }}>
-          <Typography>Active: Valid till Oct 14, 2022</Typography>
+        <Grid item xs={6} style={styles.gridContainer}>
+          <Typography sx={{fontSize:'14px'}}>
+            <span style={styles.activeSpan}>Active:</span> Valid till Oct 14,
+            2022
+          </Typography>
         </Grid>
       </Grid>
-      <Typography  sx={{ color: "#3f51b5", mt: 2,mb:2,fontSize:'17px'}}>
-        Shipment Details
-      </Typography>
-      <Grid container >
+
+      <Typography sx={styles.shipmentDetails}>Shipment Details</Typography>
+
+      <Grid container>
         <Grid item xs={9}>
-        <ShipmentDetails></ShipmentDetails>
+          <ShipmentDetails />
         </Grid>
         <Grid item xs={3}>
-        <RateEngineContainerDetail></RateEngineContainerDetail>
+          <RateEngineContainerDetail />
         </Grid>
-      
       </Grid>
-      
     </div>
   );
 }
-
-
